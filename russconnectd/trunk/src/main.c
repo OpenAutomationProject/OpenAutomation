780bafe123e99e462e289e5c793ec1f313761481
makki1 <Makki1@users.noreply.github.com>
2011-10-20 23:41:50 +0000

    prepared autoconf-option without-eib; added keypad-id for C5; fixed sendOnStartUp; withdraw .tar.gz - this will go under sf/oa../files; Note: OpenWRT-Makefile will also be withdrawn here once settled&committed to upstream

--- russconnectd/trunk/src/main.c
+++ russconnectd/trunk/src/main.c
@@ -26,7 +26,7 @@
 #include <syslog.h>
 #include <string.h>
 #include <signal.h>
-#include <assert.h>
+//#include <assert.h>
 
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -38,14 +38,13 @@
 
 #define DEBUG 1
 #define DAEMON_NAME "russconnectd"
-#define USAGESTRING "\n\t-d\tRun as daemon/No debug output\n\t-p <pidfile>\tPID-filename\n\t-i <ip:port>\tIP-Address:Port to send UDP-packets to russound\n\t-l <UDP-port>\tUDP port to listen on\n\t-a <KNX address>\tKNX start-address (see README)\n\t-z <number>\tNumber of Zones to support\n\t-u <eib url>\tURL to conatct eibd like localo:/tmp/eib or ip:192.168.0.101\n\t-s\t(Optional) send all values to KNX on startup of daemon\n"
-#define NUM_THREADS 2
+#define USAGESTRING "\n\t-d\tRun as daemon/No debug output\n\t-p <pidfile>\tPID-filename\n\t-i <ip:port>\tIP-Address:Port to send UDP-packets to russound\n\t-l <UDP-port>\tUDP port to listen on\n\t-a <KNX address>\tKNX start-address (see README)\n\t-z <number>\tNumber of Zones to support\n\t-u <eib url>\tURL to conatct eibd like localo:/tmp/eib or ip:192.168.0.101\n\t-s\t(Optional) send all values to KNX on startup of daemon\n\t-k 0x71\t(Optional) Keypad ID - defaults to 0x70 for CAx - use 0x71 for C5"
+#define NUM_THREADS 3
 #define MAX_ZONES 31
 #define RETRY_TIME 5
 #define BUFLEN 1024
 #define POLLING_INTERVAL 10
 #define ZONES_PER_CONTROLLER 6
-#define RUSS_KEYPAD_ID 0x70
 
 pthread_mutex_t zonelock = PTHREAD_MUTEX_INITIALIZER; 
 pthread_mutex_t initlock = PTHREAD_MUTEX_INITIALIZER; 
@@ -81,6 +80,7 @@ int numzones = ZONES_PER_CONTROLLER;
 int pidFilehandle;
 char *pidfilename = "/var/run/russconnectd.pid";
 int sendOnStart = 0;
+long int keypadid = 0x70;
 
 //FIXME: also handle serial-port directly?
 struct sockaddr_in si_me, si_other;
@@ -140,7 +140,7 @@ char *russChecksum(char* buf, int len) {
 void *sendrussPolling(unsigned char zone) {
 	syslog(LOG_DEBUG, "polling zone %d",zone);
 	pthread_mutex_trylock(&standbylock);
-	char buf_onvol[25] = { 0xF0, 0, 0, 0x7F, 0, 0, RUSS_KEYPAD_ID, 0x01, 0x05, 0x02, 0, 0, 0, 0x04, 0, 0, 0, 0xF7 };
+	char buf_onvol[25] = { 0xF0, 0, 0, 0x7F, 0, 0, keypadid, 0x01, 0x05, 0x02, 0, 0, 0, 0x04, 0, 0, 0, 0xF7 };
 	buf_onvol[1] = zone/ZONES_PER_CONTROLLER;
 	buf_onvol[11] = zone%ZONES_PER_CONTROLLER;
 	buf_onvol[16] = (int) russChecksum (buf_onvol,18-2);
@@ -148,7 +148,7 @@ void *sendrussPolling(unsigned char zone) {
 		syslog(LOG_WARNING,"sendto failed!");
 	usleep(20*1000); //FIXME: throttle a little (20ms)
 
-	char buf_getzone[25] = { 0xF0, 0, 0, 0x7F, 0, 0, RUSS_KEYPAD_ID, 0x01, 0x04, 0x02, 0, 0, 0x07, 0, 0, 0, 0xF7 };
+	char buf_getzone[25] = { 0xF0, 0, 0, 0x7F, 0, 0, keypadid, 0x01, 0x04, 0x02, 0, 0, 0x07, 0, 0, 0, 0xF7 };
 	buf_getzone[1] = zone/ZONES_PER_CONTROLLER;
 	buf_getzone[11] = zone%ZONES_PER_CONTROLLER;
 	buf_getzone[15] = (int) russChecksum (buf_getzone,17-2);
@@ -167,10 +167,10 @@ void *sendrussFunc(int controller, int zone, int func, int val) {
 	//block here until something is received from russound (it's turned off)
 	pthread_mutex_lock(&standbylock);
 	pthread_mutex_unlock(&standbylock);
-	char buf_msg1[25] = { 0, 0, 0, 0x7F, 0, 0, RUSS_KEYPAD_ID, 0x05, 0x02, 0x02, 0, 0, 0xF1, 0x23, 0, 0, 0, 0, 0, 0x01, 0, 0xF7 };
+	char buf_msg1[25] = { 0, 0, 0, 0x7F, 0, 0, keypadid, 0x05, 0x02, 0x02, 0, 0, 0xF1, 0x23, 0, 0, 0, 0, 0, 0x01, 0, 0xF7 };
 	buf_msg1[1] = controller;
 	buf_msg1[17] = zone;
-	char buf_msg2[25] = { 0, 0, 0, 0x7F, 0, 0, RUSS_KEYPAD_ID, 0, 0x05, 0x02, 0, 0, 0, 0, 0, 0, 0, 0x01, 0, 0x01, 0, 0, 0, 0xF7 };
+	char buf_msg2[25] = { 0, 0, 0, 0x7F, 0, 0, keypadid, 0, 0x05, 0x02, 0, 0, 0, 0, 0, 0, 0, 0x01, 0, 0x01, 0, 0, 0, 0xF7 };
 	buf_msg2[1] = controller;
 	buf_msg2[11] = zone;
 
@@ -497,22 +497,22 @@ void *updateZone(unsigned char num, unsigned char val, int func) {
 	switch (func) {
 			case 1:
 				zones[num].zonepower =val;
-				if (zones[num].inited)
+				if (zones[num].inited || sendOnStart)
 					sendKNXdgram (0x80,1,(knxstartaddress+30)+(zone*40)+(controller*256),val);
 				break;
 			case 2:
 				zones[num].srcid =val;
-				if (zones[num].inited)
+				if (zones[num].inited || sendOnStart)
 					sendKNXdgram (0x80,51,(knxstartaddress+31)+(zone*40)+(controller*256),val);
 				break;
 			case 3:
 				zones[num].volume =val;
-				if (zones[num].inited)
+				if (zones[num].inited || sendOnStart)
 					sendKNXdgram (0x80,5,(knxstartaddress+32)+(zone*40)+(controller*256),val*2);
 				break;
 			case 4:
 				zones[num].bass =val;
-				if (zones[num].inited) {
+				if (zones[num].inited || sendOnStart) {
 					if (val<10)
 						sendKNXdgram (0x80,51,(knxstartaddress+33)+(zone*40)+(controller*256),val-10+256);
 					else
@@ -521,7 +521,7 @@ void *updateZone(unsigned char num, unsigned char val, int func) {
 				break;
 			case 5:
 				zones[num].treble =val;
-				if (zones[num].inited) {
+				if (zones[num].inited || sendOnStart) {
 					if (val<10)
 						sendKNXdgram (0x80,51,(knxstartaddress+34)+(zone*40)+(controller*256),val-10+256);
 					else
@@ -530,12 +530,12 @@ void *updateZone(unsigned char num, unsigned char val, int func) {
 				break;
 			case 6:
 				zones[num].loudness =val;
-				if (zones[num].inited)
+				if (zones[num].inited || sendOnStart)
 					sendKNXdgram (0x80,1,(knxstartaddress+35)+(zone*40)+(controller*256),val);
 				break;
 			case 7:
 				zones[num].balance =val;
-				if (zones[num].inited) {
+				if (zones[num].inited || sendOnStart) {
 					if (val<10)
 						sendKNXdgram (0x80,51,(knxstartaddress+36)+(zone*40)+(controller*256),val-10+256);
 					else
@@ -544,17 +544,17 @@ void *updateZone(unsigned char num, unsigned char val, int func) {
 				break;
 			case 8:
 				zones[num].partymode =val;
-				if (zones[num].inited)
+				if (zones[num].inited || sendOnStart)
 					sendKNXdgram (0x80,1,(knxstartaddress+37)+(zone*40)+(controller*256),val);
 				break;
 			case 9:
 				zones[num].dnd =val;
-				if (zones[num].inited)
+				if (zones[num].inited || sendOnStart)
 					sendKNXdgram (0x80,1,(knxstartaddress+38)+(zone*40)+(controller*256),val);
 				break;
 			case 10:
 				zones[num].onvolume =val;
-				if (zones[num].inited)
+				if (zones[num].inited || sendOnStart)
 					sendKNXdgram (0x80,5,(knxstartaddress+39)+(zone*40)+(controller*256),val*2);
 				break;
 			default:
@@ -570,26 +570,24 @@ void *parseRussMsg(unsigned char* buf, int len) {
 			syslog(LOG_DEBUG,"russ Controller:%d Zone:%d Status:%d src:%d vol:%d bass:%d treb:%d loud:%d bal:%d sys:%d shrsrc:%d party:%d,DnD:%d\n",
 			       buf[4],buf[12],buf[20],buf[21],buf[22],buf[23],buf[24],buf[25],buf[26],buf[27],buf[28],buf[29],buf[30]);
 		buf[12] = (buf[4]*ZONES_PER_CONTROLLER)+buf[12]; //controller + zonenumber
-		if (sendOnStart)
-			zones[buf[12]].inited = 1;
 		
-		if (buf[20] != zones[buf[12]].zonepower)
+		if (buf[20] != zones[buf[12]].zonepower || (sendOnStart && !zones[buf[12]].inited))
 			updateZone(buf[12],buf[20],1);
-		if (buf[21] != zones[buf[12]].srcid)
+		if (buf[21] != zones[buf[12]].srcid  || (sendOnStart && !zones[buf[12]].inited))
 			updateZone(buf[12],buf[21],2);
-		if (buf[22] != zones[buf[12]].volume)
+		if (buf[22] != zones[buf[12]].volume || (sendOnStart && !zones[buf[12]].inited))
 			updateZone(buf[12],buf[22],3);
-		if (buf[23] != zones[buf[12]].bass)
+		if (buf[23] != zones[buf[12]].bass || (sendOnStart && !zones[buf[12]].inited))
 			updateZone(buf[12],buf[23],4);
-		if (buf[24] != zones[buf[12]].treble)
+		if (buf[24] != zones[buf[12]].treble || (sendOnStart && !zones[buf[12]].inited))
 			updateZone(buf[12],buf[24],5);
-		if (buf[25] != zones[buf[12]].loudness)
+		if (buf[25] != zones[buf[12]].loudness || (sendOnStart && !zones[buf[12]].inited))
 			updateZone(buf[12],buf[25],6);
-		if (buf[26] != zones[buf[12]].balance)
+		if (buf[26] != zones[buf[12]].balance || (sendOnStart && !zones[buf[12]].inited))
 			updateZone(buf[12],buf[26],7);
-		if (buf[29] != zones[buf[12]].partymode)
+		if (buf[29] != zones[buf[12]].partymode || (sendOnStart && !zones[buf[12]].inited))
 			updateZone(buf[12],buf[29],8);
-		if (buf[30] != zones[buf[12]].dnd)
+		if (buf[30] != zones[buf[12]].dnd || (sendOnStart && !zones[buf[12]].inited))
 			updateZone(buf[12],buf[30],9);
 		zones[buf[12]].inited = 1;
 	} else if ((len==24) && (buf[0]==0xF0) && (buf[9]==0x05) && (buf[13]==0x00)) { //zone turn-on volume
@@ -597,7 +595,7 @@ void *parseRussMsg(unsigned char* buf, int len) {
 			syslog(LOG_DEBUG,"russ Controller:%d Zone:%d TurnOnVolume:%d",
 			       buf[4],buf[12],buf[21]);
 		buf[12] = (buf[4]*ZONES_PER_CONTROLLER)+buf[12]; //controller + zonenumber
-		if (buf[21] != zones[buf[12]].onvolume)
+		if (buf[21] != zones[buf[12]].onvolume || (sendOnStart && !zones[buf[12]].inited))
 			updateZone(buf[12],buf[21],10);
 	} else {
 		//FIXME: just for debugging
@@ -740,7 +738,7 @@ int main(int argc, char **argv) {
 	char *p;
 	char pidstr[255];
 	
-	while ((c = getopt (argc, argv, "dp:i:l:a:z:u:s")) != -1)
+	while ((c = getopt (argc, argv, "dp:i:l:a:z:u:sk:")) != -1)
 		switch (c) {
 			case 'd':
 				daemonize = 1;
@@ -754,8 +752,8 @@ int main(int argc, char **argv) {
 			case 'i':
 				p = strtok(optarg,":");
 				russipaddr = p;
-				p = strtok (NULL, ":");
-				russport = atoi(p);
+				if ((p = strtok (NULL, ":")))
+					russport = atoi(p);
 				break;
 			case 'l':
 				listenport = atoi(optarg);
@@ -768,6 +766,9 @@ int main(int argc, char **argv) {
 				break;
 			case 'u':
 				eibd_url = optarg;
+				break;
+			case 'k':
+				keypadid = strtol(optarg,NULL,16);
 				break;
 			case '?':
 				//FIXME: check arguments better, print_usage
@@ -786,8 +787,8 @@ int main(int argc, char **argv) {
     if (!daemonize) {
 	    setlogmask(LOG_UPTO(LOG_DEBUG));
 		openlog(DAEMON_NAME, LOG_CONS | LOG_NDELAY | LOG_PERROR | LOG_PID, LOG_USER);
-        syslog(LOG_DEBUG, "startup with debug; Russ-IP: %s:%d, listenport %d, pidfile: %s, start address: %d, number of zones: %d, eibd: %s", 
-               russipaddr, russport, listenport, pidfilename, knxstartaddress, numzones, eibd_url);
+        syslog(LOG_DEBUG, "startup with debug; Russ-IP: %s:%d, listenport %d, pidfile: %s, start address: %d, number of zones: %d, eibd: %s SendonStartup: %d KeypadID: %d", 
+               russipaddr, russport, listenport, pidfilename, knxstartaddress, numzones, eibd_url, sendOnStart, keypadid);
 	} else {
 	    setlogmask(LOG_UPTO(LOG_INFO));
 		openlog(DAEMON_NAME, LOG_CONS, LOG_USER);
