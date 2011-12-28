c86b54e4a7dabc1e14afa56ed0fcd74b551f9441
makki1 <Makki1@users.noreply.github.com>
2011-12-28 00:15:30 +0000

    Change cfgmaker to new infotrigger, various small adjustments, there is still a HEAVY bug in setting Bass/Treble if no absolute values are used though, be aware..

--- russconnectd/trunk/cfgmaker.pl
+++ russconnectd/trunk/cfgmaker.pl
@@ -57,6 +57,16 @@ open VISU3, ">", "visu_snipplet_room_pages_all.xml"  or die $!;
 
 print VISU1 "    <page name=\"$NamePrefix\">\n";
 
+print VISU2 "<!-- Change SourceNames in script and insert this on top between <mappings> and </mappings> -->\n";
+print VISU2 "      <mapping name=\"RussoundSRC\">\n"
+."        <entry value=\"0\">$SourceNames[0]</entry>\n"
+."        <entry value=\"1\">$SourceNames[1]</entry>\n"
+."        <entry value=\"2\">$SourceNames[2]</entry>\n"
+."        <entry value=\"3\">$SourceNames[3]</entry>\n"
+."        <entry value=\"4\">$SourceNames[4]</entry>\n"
+."        <entry value=\"5\">$SourceNames[5]</entry>\n"
+."      </mapping>\n\n\n";
+
 # Globals
 $conf->{addr2str($startga+1,1)}->{'name'} = $NamePrefix . " " . $gfuncnames[0];
 $conf->{addr2str($startga+1,1)}->{'DPTSubId'} = 1.001;
@@ -93,7 +103,7 @@ for (my $zone=0;$zone<$opts{z};$zone++) {
         print ESF "$NamePrefix.Controller$ctrl." . addr2str($basega+$i+20,1) . "\t" . "$NamePrefix $zonenames[$zone] C" . ($ctrl+1) . "-Z" . ($czone+1) . " $statenames[$i]" . "\t" . $EISmap{$statedpts[$i]} . "\tLow\n";
         print XML "\t\t<object id=\"$zonenames[$zone]_C" . ($ctrl+1) . "_Z" . ($czone+1) . "_" . "$statenames[$i]\" gad=\"" . addr2str($basega+$i+20,1) . "\" type=\"$statedpts[$i]\">" . "$NamePrefix $zonenames[$zone] (C" . ($ctrl+1) . "/Z" . ($czone+1) . ") $statenames[$i]" . "</object>\n";
     }
-    print VISU1 '      <switch mapping="OnOff" styling="RedGreen">'
+    print VISU1 '      <switch mapping="OnOff" styling="GreenRed">'
              ."\n        <label>$zonenames[$zone]</label>\n"
                .'        <address transform="DPT:1.001" readonly="false" type="">' . addr2str($basega,1) . "</address>\n"
                .'        <address transform="DPT:1.001" readonly="true" type="">' .  addr2str($basega+20,1) . "</address>\n"
@@ -103,74 +113,71 @@ for (my $zone=0;$zone<$opts{z};$zone++) {
                .'        <address transform="DPT:5.001" readonly="false" type="">' . addr2str($basega+2,1) . "</address>\n"
                .'        <address transform="DPT:5.001" readonly="true" type="">' .  addr2str($basega+22,1) . "</address>\n"
                ."      </slide>\n";
+               
     print VISU2 "    <page name=\"$NamePrefix $zonenames[$zone]\">\n";
-    print VISU2 '      <switch mapping="OnOff" styling="RedGreen">'
+    print VISU2 '      <switch mapping="OnOff" styling="GreenRed">'
              ."\n        <label>Power</label>\n"
                .'        <address transform="DPT:1.001" readonly="false" type="">' . addr2str($basega,1) . "</address>\n"
                .'        <address transform="DPT:1.001" readonly="true" type="">' .  addr2str($basega+20,1) . "</address>\n"
                ."      </switch>\n";
     print VISU2 '      <slide min="0" max="100">'
-             ."\n        <label>Vol</label>\n"
+             ."\n        <label>Volume</label>\n"
                .'        <address transform="DPT:5.001" readonly="false" type="">' . addr2str($basega+2,1) . "</address>\n"
                .'        <address transform="DPT:5.001" readonly="true" type="">' .  addr2str($basega+22,1) . "</address>\n"
                ."      </slide>\n";
-    print VISU2 '      <info format="">'
-             ."\n        <label>Source</label>\n"
-               .'        <address transform="DPT:5.010" readonly="false" type="">' . addr2str($basega+1,1) . "</address>\n"
-               .'        <address transform="DPT:5.010" readonly="true" type="">' .  addr2str($basega+21,1) . "</address>\n"
-               ."      </info>\n";
-    print VISU2 "      <multitrigger button1label=\"$SourceNames[0]\" button1value=\"0\" button2label=\"$SourceNames[1]\" button2value=\"1\" button3label=\"$SourceNames[2]\" button3value=\"2\" button4label=\"$SourceNames[3]\" button4value=\"3\">"
-             ."\n        <label>Source</label>\n"
-               .'        <address transform="DPT:5.010" readonly="false" type="">' . addr2str($basega+1,1) . "</address>\n"
-               .'        <address transform="DPT:5.010" readonly="true" type="">' .  addr2str($basega+21,1) . "</address>\n"
-               ."      </multitrigger>\n";
-    print VISU2 "      <multitrigger button1label=\"$SourceNames[4]\" button1value=\"4\" button2label=\"$SourceNames[5]\" button2value=\"5\">"
-             ."\n        <label>Source</label>\n"
-               .'        <address transform="DPT:5.010" readonly="false" type="">' . addr2str($basega+1,1) . "</address>\n"
-               .'        <address transform="DPT:5.010" readonly="true" type="">' .  addr2str($basega+21,1) . "</address>\n"
-               ."      </multitrigger>\n";
-    print VISU2 '      <switch mapping="OnOff" styling="RedGreen">'
+    #$SourceNames[0]
+    print VISU2 '      <infotrigger uplabel="&gt;" upvalue="1" downlabel="&lt;" downvalue="-1" align="center" infoposition="1" change="absolute" min="0" max="5" mapping="RussoundSRC">'
+#    <infotrigger uplabel="&gt;" upvalue="1" downlabel="&lt;" downvalue="-1" align="center" infoposition="1" change="absolute" min="0" max="5" mapping="RussoundSRC">
+#      <label>Source</label>
+#      <address transform="DPT:5.010" readonly="false" variant="">10/1/11</address>
+#      <address transform="DPT:5.010" readonly="true" variant="">10/1/31</address>
+#    </infotrigger>
+             ."\n        <label>Quelle</label>\n"
+               .'        <address transform="DPT:5.010" readonly="false">' . addr2str($basega+1,1) . "</address>\n"
+               .'        <address transform="DPT:5.010" readonly="true">' .  addr2str($basega+21,1) . "</address>\n"
+               ."      </infotrigger>\n";
+    print VISU2 '      <switch mapping="OnOff" styling="GreenRed">'
              ."\n        <label>Loudness</label>\n"
                .'        <address transform="DPT:1.001" readonly="false" type="">' . addr2str($basega+5,1) . "</address>\n"
                .'        <address transform="DPT:1.001" readonly="true" type="">' .  addr2str($basega+25,1) . "</address>\n"
                ."      </switch>\n";
-    print VISU2 '      <infotrigger button1label="+" button1value="1" button2label="-" button2value="0" align="center" infoposition="1" format="%.0f %%">'
+    print VISU2 '      <infotrigger button1label="+" button1value="1" button2label="-" button2value="0" align="center" infoposition="1" format="%.0f %% ">'
              ."\n        <label>Volume</label>\n"
-               .'        <address transform="DPT:1.008" readonly="false" type="">' . addr2str($basega+12,1) . "</address>\n"
-               .'        <address transform="DPT:5.001" readonly="true" type="">' . addr2str($basega+2,1) . "</address>\n"
-               .'        <address transform="DPT:5.001" readonly="true" type="">' . addr2str($basega+22,1) . "</address>\n"
+               .'        <address transform="DPT:1.008" readonly="false" variant="button">' . addr2str($basega+12,1) . "</address>\n"
+               .'        <address transform="DPT:5.001" readonly="true" variant="">' . addr2str($basega+2,1) . "</address>\n"
+               .'        <address transform="DPT:5.001" readonly="true" variant="">' . addr2str($basega+22,1) . "</address>\n"
                ."      </infotrigger>\n";
-    print VISU2 '      <info format="%.0f %%">'
-             ."\n        <label>TurnOn Vol</label>\n"
+    print VISU2 '      <infotrigger align="center" infoposition="1" upvalue="2" downvalue="-2" change="absolute" format="%.0f %%">'
+             ."\n        <label>TurnOn Volume</label>\n"
                .'        <address transform="DPT:5.001" readonly="false" type="">' . addr2str($basega+9,1) . "</address>\n"
                .'        <address transform="DPT:5.001" readonly="true" type="">' .  addr2str($basega+29,1) . "</address>\n"
-               ."      </info>\n";
-    print VISU2 '      <slide min="0" max="100">'
-             ."\n        <label>TurnOn Vol</label>\n"
-               .'        <address transform="DPT:5.001" readonly="false" type="">' . addr2str($basega+9,1) . "</address>\n"
-               .'        <address transform="DPT:5.001" readonly="true" type="">' .  addr2str($basega+29,1) . "</address>\n"
-               ."      </slide>\n";
-    print VISU2 '      <slide min="-10" max="10">'
+               ."      </infotrigger>\n";
+#    print VISU2 '      <slide min="0" max="100">'
+#             ."\n        <label>TurnOn Vol</label>\n"
+#               .'        <address transform="DPT:5.001" readonly="false" type="">' . addr2str($basega+9,1) . "</address>\n"
+#               .'        <address transform="DPT:5.001" readonly="true" type="">' .  addr2str($basega+29,1) . "</address>\n"
+#               ."      </slide>\n";
+    print VISU2 '      <infotrigger align="center" infoposition="1" upvalue="1" downvalue="-1" change="absolute" min="-10" max="10">'
              ."\n        <label>Bass</label>\n"
                .'        <address transform="DPT:6.001" readonly="false" type="">' . addr2str($basega+3,1) . "</address>\n"
                .'        <address transform="DPT:6.001" readonly="true" type="">' .  addr2str($basega+23,1) . "</address>\n"
-               ."      </slide>\n";
-    print VISU2 '      <slide min="-10" max="10">'
+               ."      </infotrigger>\n";
+    print VISU2 '      <infotrigger align="center" infoposition="1" upvalue="1" downvalue="-1" change="absolute" min="-10" max="10">'
              ."\n        <label>Treble</label>\n"
                .'        <address transform="DPT:6.001" readonly="false" type="">' . addr2str($basega+4,1) . "</address>\n"
                .'        <address transform="DPT:6.001" readonly="true" type="">' .  addr2str($basega+24,1) . "</address>\n"
-               ."      </slide>\n";
-    print VISU2 '      <slide min="-10" max="10">'
+               ."      </infotrigger>\n";
+    print VISU2 '      <infotrigger align="center" infoposition="1" upvalue="1" downvalue="-1" change="absolute" min="-10" max="10">'
              ."\n        <label>Balance</label>\n"
                .'        <address transform="DPT:6.001" readonly="false" type="">' . addr2str($basega+6,1) . "</address>\n"
                .'        <address transform="DPT:6.001" readonly="true" type="">' .  addr2str($basega+26,1) . "</address>\n"
-               ."      </slide>\n";
-    print VISU2 '      <switch mapping="OnOff" styling="RedGreen">'
+               ."      </infotrigger>\n";
+    print VISU2 '      <switch mapping="OnOff" styling="GreenRed">'
              ."\n        <label>Party</label>\n"
                .'        <address transform="DPT:1.001" readonly="false" type="">' . addr2str($basega+7,1) . "</address>\n"
                .'        <address transform="DPT:1.001" readonly="true" type="">' .  addr2str($basega+27,1) . "</address>\n"
                ."      </switch>\n";
-    print VISU2 '      <switch mapping="OnOff" styling="RedGreen">'
+    print VISU2 '      <switch mapping="OnOff" styling="GreenRed">'
              ."\n        <label>DnD</label>\n"
                .'        <address transform="DPT:1.001" readonly="false" type="">' . addr2str($basega+8,1) . "</address>\n"
                .'        <address transform="DPT:1.001" readonly="true" type="">' .  addr2str($basega+28,1) . "</address>\n"
