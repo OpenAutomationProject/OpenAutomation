3bb63a4ea1e4f9aa0b71b8f4f1c5512462e4ecc2
Roland Jax <yuhu-@users.noreply.github.com>
2013-02-17 15:49:15 +0000

    Add missing license info to csv.c

--- ebus/ebusd/tools/csv.c
+++ ebus/ebusd/tools/csv.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) Roland Jax 2012-2013 <roland.jax@liwest.at>
+ * crc calculations from http://www.mikrocontroller.net/topic/75698
+ *
+ * This file is part of ebusd.
+ *
+ * ebusd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ebusd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ebusd. If not, see http://www.gnu.org/licenses/.
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,9 +28,6 @@
 
 #include "log.h"
 #include "ebus-bus.h"
-
-//~ #define YES  1
-//~ #define NO   0
 
 /* global variables */
 const char *progname;
